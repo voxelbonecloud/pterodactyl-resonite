252b4c0a58cafc36835868744482a21cb5b9f127
bredo <bredo@bredo.tech>
2024-07-12 10:28:06 +1200

    cleanup: remove build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export IMAGE_NAME="voxelbone.cloud/resonite-pri:latest"
-#export IMAGE_NAME="ghcr.io/voxelbonecloud/pterodactyl-resonite"
-
-docker buildx build --pull -t $IMAGE_NAME .
-
-#docker save $IMAGE_NAME | bzip2 | pv | ssh HOST_HERE docker load
