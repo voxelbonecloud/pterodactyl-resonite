c96bf31e4642856a4bac5fcfc252b53f838cfa46
bredo <bredo@bredo.tech>
2024-11-13 12:32:12 +1300

    entrypoint: tweak to validate
    steam's being a pain right now, seeing if i can get it to exist properly.

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,7 +33,7 @@ export INTERNAL_IP
 cd /home/container || exit 1
 
 # Update Resonite headless
-./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASSWORD} ${STEAM_AUTH} +force_install_dir /home/container +app_update 2519830 -beta headless -betapassword ${BETA_CODE} +quit
+./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASSWORD} ${STEAM_AUTH} +force_install_dir /home/container +app_update 2519830 -beta headless -betapassword ${BETA_CODE} +validate +quit
 #./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASSWORD} ${STEAM_AUTH} +force_install_dir /home/container +app_update 2519830 -beta headless +quit
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
 # variable format of "${VARIABLE}" before evaluating the string and automatically
