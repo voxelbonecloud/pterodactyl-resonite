e4e6b96bfc2f141b98d16b8d0974e11dbc0c22c4
bredo <bredo@bredo.tech>
2024-11-13 12:33:25 +1300

    Update entrypoint.sh
    remove broken INTERNAL_IP thing, i need to sort this out but for now i'm just removing it to force the build to run

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,8 +26,8 @@ TZ=${TZ:-UTC}
 export TZ
 
 # Set environment variable that holds the Internal Docker IP
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
+# INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+#export INTERNAL_IP
 
 # Switch to the container's working directory
 cd /home/container || exit 1
